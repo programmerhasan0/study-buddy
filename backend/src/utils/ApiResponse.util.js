6a6abbe5dc4c9e773ce00385447b2187ab64c7f4
programmerhasan0 <programmerhasan0@gmail.com>
2025-09-27 13:26:32 +0600

    removed dot(.) from the url

--- backend/src/utils/ApiResponse.util.js
+++ backend/src/utils/ApiResponse.util.js
@@ -29,7 +29,7 @@ class ApiResponse {
             httpOnly: true,
             secure: true,
             sameSite: 'none',
-            domain: '.study-buddy-ph0.web.app',
+            domain: 'study-buddy-ph0.web.app',
             maxAge: 1000 * 60 * 60 * 24,
         });
         return this;
